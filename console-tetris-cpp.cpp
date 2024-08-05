74baf9ed05ba5e36c2f633772508d855dab29420
vlifirenko <vladislav.lifirenko@gmail.com>
2024-08-05 15:21:14 +0300

    Prepare field and figures

--- console-tetris-cpp.cpp
+++ console-tetris-cpp.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
+using namespace std;
+
+#include <Windows.h>
+
+wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char* pField = nullptr;
+
+int nScreenWidth = 80;
+int nScreenHeight = 30;
+
+int Rotate(int px, int py, int r)
+{
+	switch (r % 4)
+	{
+	case 0:
+		return py * 4 + px;				// 0 deg
+	case 1:
+		return 12 + py - (px * 4);		// 90 deg
+	case 2:
+		return 15 - (py * 4) - px;		// 180 deg
+	case 3:
+		return 3 - py + (px * 4);		// 270 deg
+	}
+
+	return 0;
+}
 
 int main()
 {
+   // create assets
+	tetromino[0].append(L"..X...X...X...X.");		// tetronimos 4x4
+	tetromino[1].append(L"..X..XX...X.....");
+	tetromino[2].append(L".....XX..XX.....");
+	tetromino[3].append(L"..X..XX..X......");
+	tetromino[4].append(L".X...XX...X.....");
+	tetromino[5].append(L".X...X...XX.....");
+	tetromino[6].append(L"..X...X..XX.....");
+
+	pField = new unsigned char[nFieldWidth * nFieldHeight];
+	for (int x = 0; x < nFieldWidth; x++)
+		for (int y = 0; y < nFieldHeight; y++)
+			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
+		screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	bool bGameOver = false;
+
+	while (!bGameOver)
+	{
+		// draw field
+		for (int x = 0; x < nFieldWidth; x++)
+			for (int y = 0; y < nFieldHeight; y++)
+				screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+
    return 0;
 }
